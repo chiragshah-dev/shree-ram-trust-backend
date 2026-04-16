a3561e15639cd94c2adf957a1fb8c1cce943d3f1
dhartiodedara27 <dharti.odedara@aspireedge.com>
2026-04-16 12:39:58 +0530

    fix(schedule): update cron job frequency to every minute and adjust output path

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,9 @@
 # # config/schedule.rb
 
-set :output, "log/cron.log"
+
+set :output, "/home/developer/projects/shree-ram-trust-backend/log/cron.log"
 set :environment, "development"
-
-env :PATH, ENV['PATH']
-
+set :job_template, "/bin/bash -l -c 'source /home/developer/.rvm/scripts/rvm && rvm use 3.2.2@shreeram && export RUBYOPT=\"-W0\" && :job'"
 
 every 30.minute do
   rake "task_reminder:send"
